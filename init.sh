15fc869950db480922a0f1534393b1c578b244d3
suarezalvarez <martin.suarezalvarez1@gmail.com>
2026-09-18 21:44:32 +0200

    Update gitignore

--- init.sh
+++
@@ -1,5 +0,0 @@
-export VENV="$HOME/minstallation/.venvs/KKIK_parse"
-echo "Variable "VENV" refers to $VENV"
-export  UV_PROJECT_ENVIRONMENT=$VENV
-source "$VENV/bin/activate"
-echo "uv project $VIRTUAL_ENV has been activated"
